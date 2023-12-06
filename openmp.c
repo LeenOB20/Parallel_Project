91cecb8d18b7de364c1ce14205acb040ab0cc224
LeenOB20 <132571161+LeenOB20@users.noreply.github.com>
2023-12-06 10:29:29 +0200

    Update openmp.c

--- openmp.c
+++ openmp.c
@@ -9,48 +9,27 @@
 #define N 256
 #define THRESH 100
 
-void sobel_edge_detection_serial(float input[M][N], float output[M][N]) {
-     int desired_threads = 2; // Change this value as needed
-    omp_set_num_threads(desired_threads);
-    for (int i = 1; i < M - 1; i++) {
-        for (int j = 1; j < N - 1; j++) {
-            float gradient_h = (-1.0 * input[i - 1][j - 1]) + (1.0 * input[i + 1][j - 1]) +
-                               (-2.0 * input[i - 1][j]) + (2.0 * input[i + 1][j]) +
-                               (-1.0 * input[i - 1][j + 1]) + (1.0 * input[i + 1][j + 1]);
-
-            float gradient_v = (-1.0 * input[i - 1][j - 1]) + (-2.0 * input[i][j - 1]) +
-                               (-1.0 * input[i + 1][j - 1]) + (1.0 * input[i - 1][j + 1]) +
-                               (2.0 * input[i][j + 1]) + (1.0 * input[i + 1][j + 1]);
-
-            float gradient = sqrt((gradient_h * gradient_h) + (gradient_v * gradient_v));
-
-            if (gradient < THRESH) {
-                output[i][j] = 0;
-            } else {
-                output[i][j] = 255;
-            }
-        }
-    }
-}
-
 void sobel_edge_detection_parallel(float input[M][N], float output[M][N]) {
-    #pragma omp parallel for collapse(2)
-    for (int i = 1; i < M - 1; i++) {
-        for (int j = 1; j < N - 1; j++) {
-            float gradient_h = (-1.0 * input[i - 1][j - 1]) + (1.0 * input[i + 1][j - 1]) +
-                               (-2.0 * input[i - 1][j]) + (2.0 * input[i + 1][j]) +
-                               (-1.0 * input[i - 1][j + 1]) + (1.0 * input[i + 1][j + 1]);
+    #pragma omp parallel
+    {
+        #pragma omp for collapse(2)
+        for (int i = 1; i < M - 1; i++) {
+            for (int j = 1; j < N - 1; j++) {
+                float gradient_h = (-1.0 * input[i - 1][j - 1]) + (1.0 * input[i + 1][j - 1]) +
+                                   (-2.0 * input[i - 1][j]) + (2.0 * input[i + 1][j]) +
+                                   (-1.0 * input[i - 1][j + 1]) + (1.0 * input[i + 1][j + 1]);
 
-            float gradient_v = (-1.0 * input[i - 1][j - 1]) + (-2.0 * input[i][j - 1]) +
-                               (-1.0 * input[i + 1][j - 1]) + (1.0 * input[i - 1][j + 1]) +
-                               (2.0 * input[i][j + 1]) + (1.0 * input[i + 1][j + 1]);
+                float gradient_v = (-1.0 * input[i - 1][j - 1]) + (-2.0 * input[i][j - 1]) +
+                                   (-1.0 * input[i + 1][j - 1]) + (1.0 * input[i - 1][j + 1]) +
+                                   (2.0 * input[i][j + 1]) + (1.0 * input[i + 1][j + 1]);
 
-            float gradient = sqrt((gradient_h * gradient_h) + (gradient_v * gradient_v));
+                float gradient = sqrt((gradient_h * gradient_h) + (gradient_v * gradient_v));
 
-            if (gradient < THRESH) {
-                output[i][j] = 0;
-            } else {
-                output[i][j] = 255;
+                if (gradient < THRESH) {
+                    output[i][j] = 0;
+                } else {
+                    output[i][j] = 255;
+                }
             }
         }
     }
@@ -68,14 +47,7 @@ int main() {
 
     printf("Width: %d\nHeight: %d\n", M, N);
 
-    double start_time_serial = omp_get_wtime();
-
-    // Sobel edge detection (Serial)
-    sobel_edge_detection_serial(input, output);
-
-    double end_time_serial = omp_get_wtime();
-
-    printf("Serial Execution Time: %f seconds\n", end_time_serial - start_time_serial);
+    double start_time_total = omp_get_wtime();
 
     // Clear output array for parallel execution
     memset(output, 0, sizeof(output));
@@ -91,8 +63,20 @@ int main() {
 
     // Write output image
     pgmwrite(output_filename, output, M, N);
-      int num_threads = omp_get_max_threads();
-      printf("num_threads = %d",num_threads);
+
+    double end_time_total = omp_get_wtime();
+
+    printf("Total Execution Time: %f seconds\n", end_time_total - start_time_total);
+
+    // Get the number of threads
+    int num_threads;
+    #pragma omp parallel
+    {
+        #pragma omp master
+        num_threads = omp_get_num_threads();
+    }
+
+    printf("Number of threads: %d\n", num_threads);
 
     return 0;
 }
